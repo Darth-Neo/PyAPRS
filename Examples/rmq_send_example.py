579201925b0cfc2457d96295933566074c903843
DarthNeo <morrisspid.james@hotmail.com>
2016-08-28 20:01:51 -0400

    And so I said...

--- Examples/rmq_send_example.py
+++ Examples/rmq_send_example.py
@@ -2,17 +2,20 @@
 import pika
 
 if __name__ == u"__main__":
-    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-    channel = connection.channel()
 
-    channel.queue_declare(queue="hello")
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters("192.168.1.125"))
+        channel = connection.channel()
 
-    channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
+        channel.queue_declare(queue="hello")
 
-    print(" [x] Sent 'Hello World!'")
+        channel.basic_publish(exchange="", routing_key="hello", body="Hello World!")
 
-    connection.close()
+        print(" [x] Sent 'Hello World!'")
 
+        connection.close()
 
+    except Exception, msg:
+        print("%s" % msg)
 
 
